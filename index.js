6410f0d680c89a9a349e1cd204f548fa808548e6
unknown <fortibrinelike@gmail.com>
2020-05-09 23:56:08 +0300

    1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function valid (form) {
     var password = form.password.value;
     if (login == "admin" && password == "admin") {
         alert("Ты ввошел в систему");
-        window.location = "https://youtube.com";          
+        window.location = "https://fortibbrine.github.io/articles.html";          
     }
     else {
         alert("Сори чувак нельзя")
