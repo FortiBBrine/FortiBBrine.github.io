00cd517a95aeab5f1a6994f87d76ebc917a3e56d
unknown <fortibrinelike@gmail.com>
2020-05-10 13:47:50 +0300

    1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function valid (form) {
     var login = form.name.value;
     var password = form.password.value;
-    if ((login == "admin" && password == "admin") || (login == "Scorpion" && password == "5040") || (login == "NIKA" && password == "4050")) {
+    if ((login == "admin" && password == "admin") || (login == "Scorpion" && password == "5040") || (login == "NIKA" && password == "4050") || (login == "Dimaproplay" && password == "88005553535")) {
         alert("Ты ввошел в систему");
         window.location = "https://fortibbrine.github.io/articles.html";          
     }
