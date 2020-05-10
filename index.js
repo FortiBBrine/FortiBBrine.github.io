b38e65aaf5361827ae12b4ee1cda4c7e6a3084eb
unknown <fortibrinelike@gmail.com>
2020-05-10 12:20:06 +0300

    1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function valid (form) {
     var login = form.name.value;
     var password = form.password.value;
-    if (login == "admin" && password == "admin") {
+    if ((login == "admin" && password == "admin") || (login == "Scorpion" && password == "5040")) {
         alert("Ты ввошел в систему");
         window.location = "https://fortibbrine.github.io/articles.html";          
     }
